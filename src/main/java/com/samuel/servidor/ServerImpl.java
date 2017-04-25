bd58ee5701b28e726aefab8c13456d074ec79a86
samukym <martinez.95a@gmail.com>
2017-04-25 15:47:30 +0200

    clases con nombres

---
+++ src/main/java/com/samuel/servidor/ServerImpl.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.samuel.servidor;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Vector;
+
+/**
+ *
+ * @author samu
+ */
+class ServerImpl extends UnicastRemoteObject implements ServerInterface {
+    
+    private Vector clientList;
+    
+    public ServerImpl() throws RemoteException {
+        super();
+        clientList = new Vector();;
+    }
+
+    @Override
+    public synchronized void registerForCallback(ClientInterface callbackClientObject)
+            throws java.rmi.RemoteException {
+        // store the callback object into the vector
+        if (!(clientList.contains(callbackClientObject))) {
+            clientList.addElement(callbackClientObject);
+            System.out.println("Registered new client ");
+        } // end if
+    }
+
+// This remote method allows an object client to
+// cancel its registration for callback
+// @param id is an ID for the client; to be used by
+// the server to uniquely identify the registered client.
+    @Override
+    public synchronized void unregisterForCallback(ClientInterface callbackClientObject)
+            throws java.rmi.RemoteException {
+        if (clientList.removeElement(callbackClientObject)) {
+            System.out.println("Unregistered client ");
+        } else {
+            System.out.println(
+                    "unregister: clientwasn't registered.");
+        }
+    }
+
+    public synchronized void doCallbacks() throws java.rmi.RemoteException {
+        
+    } // doCallbacks
+
+}// end ServerImpl class
+
--- src/main/java/com/samuel/servidor/Servidor.java
+++ src/main/java/com/samuel/servidor/Servidor.java
@@ -5,10 +5,53 @@
  */
 package com.samuel.servidor;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+
 /**
  *
  * @author samu
  */
 public class Servidor {
-    
-}
+    public static void main(String args[]) {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+        String portNum, registryURL;
+        try {
+            startRegistry(4444);
+            ServerImpl exportedObj = new ServerImpl();
+            registryURL = "rmi://localhost:4444/messenger";
+            Naming.rebind(registryURL, exportedObj);
+            System.out.println("Callback Server ready.");
+            //exportedObj.enviarNumeros();
+
+        }// end try
+        catch (Exception re) {
+            System.out.println(
+                    "Exception in HelloServer.main: " + re);
+        } // end catch
+    } // end main
+
+    //This method starts a RMI registry on the local host, if
+    //it does not already exists at the specified port number.
+    private static void startRegistry(int RMIPortNum)
+            throws RemoteException {
+        try {
+            Registry registry
+                    = LocateRegistry.getRegistry(RMIPortNum);
+            registry.list();
+            // This call will throw an exception
+            // if the registry does not already exist
+        } catch (RemoteException e) {
+            // No valid registry at that port.
+            Registry registry
+                    = LocateRegistry.createRegistry(RMIPortNum);
+        }
+    } // end startRegistry
+
+} // end class
